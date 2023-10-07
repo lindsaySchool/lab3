7d861af0622bd91275d30e9acb0d030f3df40039
lindsaySchool <r0883883@student.thomasmore.be>
2023-10-07 16:12:56 +0200

    feat: add temperature-related text to adv

--- src/App.js
+++ src/App.js
@@ -30,6 +30,25 @@ export default class App{
                 //Get the temperature from the API
                 let temp = data.current_weather.temperature;
                 document.querySelector("span").innerHTML = temp + "°C";
+                console.log(temp);
+                if(temp < 10){
+                    //Change the background of the div with id App and add after the h2 element a p element with the text: "It's cold outside, watch some tv with some snacks and your favorite coca-cola!"
+                    document.querySelector("#app").style.backgroundColor = "blue";
+                    let p = document.createElement("p");
+                    p.innerHTML = "It's cold outside, watch some tv with some snacks and your favorite coca-cola!";
+                    document.querySelector("h1").after(p);
+
+                } else if(temp > 20){
+                    document.querySelector("#app").style.backgroundColor = "red";
+                    let p = document.createElement("p");
+                    p.innerHTML = "It's hot outside, go to the beach and drink some refreshing coca-cola!";
+                    document.querySelector("h1").after(p);
+                }else{
+                    document.querySelector("#app").style.backgroundColor = "green";
+                    let p = document.createElement("p");
+                    p.innerHTML = "The sun is shining, go outside and drink some coca-cola!";
+                    document.querySelector("h1").after(p);
+                }
             })
             .catch(error => console.log(error));
     }
